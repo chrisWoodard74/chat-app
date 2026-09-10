8d0f4dbcbec2da5a9a54465ba7b15d9ff494dc62
chrisWoodard74 <chriswoodard74@hotmail.com>
2026-09-10 16:49:55 -0400

    final commit

---
+++ server/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+//Function to connect to the Database
+export const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', ()=> console.log('Database is connected'));
+        await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`)
+    } catch (error) {
+        console.log(error);
+    }
+}
